40f778a5ce3624d7f9b3ebb7cb552b9d61d3e8c8
duje <dzemunik@yahoo.com>
2024-03-12 14:22:54 +0100

    added and moved: daisyUI elements to userList.tsx and moved from /components to /pages/admin/userList (FE)

--- client/src/components/userList.tsx
+++ client/src/components/userList.tsx
@@ -3,14 +3,15 @@ import { useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { IUserData } from "../interfaces/IUserData";
 import { store } from "../store";
-import { Hourglass } from "react-loader-spinner";
+import { MessageError } from "./elements/messages/messageError";
+import { WaitingDots } from "./elements/waitingDots";
 import axios from "axios";
 
 export function UserList() {
   const [users, setUsers] = useState<IUserData[]>([]);
   const [userSearchText, setUserSearchText] = useState<string>("");
   const [onlyActive, setOnlyActive] = useState<boolean>(false);
-  const [openingLoading, setOpeningLoading] = useState<boolean>(false);
+  const [isLoaded, setIsLoaded] = useState<boolean>(false);
   const [error, setError] = useState<string>("");
 
   const { data, isChecked } = useSelector(
@@ -59,7 +60,6 @@ export function UserList() {
         return 0;
       });
       setUsers(users);
-      setOpeningLoading(true);
     } catch (error: any) {
       if (
         error?.response?.data?.status === "fail" &&
@@ -70,83 +70,116 @@ export function UserList() {
         setError("Something went wrong, please try again later.");
       }
     }
+    setIsLoaded(true);
   };
 
-  const handleUserClick = (username: string) => {
+  const handleredirectProfile = (username: string) => {
     navigate(`/profile/${username}`);
   };
 
-  if (error) {
-    return <div className="text-red-500">{error}</div>;
-  } else if (!isChecked || !openingLoading) {
+  if (!isChecked || !isLoaded) {
+    {
+      /* Loading user list data */
+    }
     return (
-      //In case user data is not checked or waiting for first fetch to be completed
-      <div>
-        <Hourglass
-          visible={true}
-          height="80"
-          width="80"
-          ariaLabel="hourglass-loading"
-          wrapperStyle={{}}
-          wrapperClass=""
-          colors={["#306cce", "#72a1ed"]}
-        />
-      </div>
+      <main>
+        <WaitingDots size={"md"} marginTop={8} />{" "}
+      </main>
     );
-  } else if (isChecked && data.username === `admin`) {
+  } else if (isChecked && isLoaded && data.username !== `admin`) {
+    {
+      /* User don't have permission */
+    }
     return (
-      <div>
-        <div>
+      <main className="mt-2 mx-auto w-[90vw]">
+        <MessageError message={"You don't have permission to see this page"} />
+      </main>
+    );
+  } else if (isChecked && isLoaded && error) {
+    {
+      /* Error message */
+    }
+    return (
+      <main className="mt-2 mx-auto w-[90vw]">
+        <MessageError message={error} />
+      </main>
+    );
+  } else if (isChecked && isLoaded && data.username === `admin`) {
+    {
+      /* User list with filters */
+    }
+    return (
+      <main className="p-2">
+        <div className="card bg-base-200 p-4 gap-2 shadow-xl mx-auto mt-2 mb-4 rounded-lg w-[90vw]">
           <form>
-            <div>
+            {/* Show only active users checkbox */}
+            <div className="flex justify-center mb-2">
               <label>
-                Show only active users:
+                Show only active users
                 <input
                   type="checkbox"
                   checked={onlyActive}
                   onChange={handleActiveUsersChange}
+                  className="ml-2"
                 />
               </label>
             </div>
-            <div>
+            {/* Search profile input */}
+            <label className="form-control w-full max-w-xs">
+              <div className="label p-0">
+                <span className="label-text">Search profile</span>
+              </div>
               <input
                 type="text"
-                placeholder="Search user..."
                 maxLength={10}
+                value={userSearchText}
                 onChange={(e) => handleInputSearch(e.target.value)}
+                className="input input-bordered w-full max-w-xs"
+              />
+            </label>{" "}
+          </form>
+          {users.length === 0 ? (
+            <div className="mt-2 mx-auto w-[90vw]">
+              {/* If no user matches the requested requirements  */}
+              <MessageError
+                message={"No user matches the requested requirements"}
               />
             </div>
-          </form>
+          ) : (
+            <table className="table">
+              {/* User list table */}
+              <thead>
+                <tr>
+                  <th className="text-center">Username</th>
+                  <th className="text-center">Email</th>
+                  <th className="text-center">Active</th>
+                </tr>
+              </thead>
+              <tbody>
+                {users &&
+                  users.map((userInfo, i) => {
+                    if (userInfo.username === `admin`) return;
+                    else
+                      return (
+                        <tr
+                          onClick={() =>
+                            handleredirectProfile(userInfo.username)
+                          }
+                          key={i}
+                        >
+                          <td className="text-center">{userInfo.username}</td>
+                          <td className="text-center">{userInfo.email}</td>
+                          <td className="text-center">
+                            {userInfo.active ? `Yes` : `No`}
+                          </td>
+                        </tr>
+                      );
+                  })}
+              </tbody>
+            </table>
+          )}
         </div>{" "}
-        {users.length === 0 ? (
-          <p>No user matches the requested requirements</p>
-        ) : (
-          <table className="userListTable">
-            <thead>
-              <tr>
-                <th>Username</th>
-                <th>Email</th>
-                <th>Active</th>
-              </tr>
-            </thead>
-            <tbody>
-              {users &&
-                users.map((userInfo, i) => {
-                  return (
-                    <tr
-                      onClick={() => handleUserClick(userInfo.username)}
-                      key={i}
-                    >
-                      <td>{userInfo.username}</td>
-                      <td>{userInfo.email}</td>
-                      <td>{userInfo.active ? `Yes` : `No`}</td>
-                    </tr>
-                  );
-                })}
-            </tbody>
-          </table>
-        )}
-      </div>
+      </main>
     );
   }
 }
