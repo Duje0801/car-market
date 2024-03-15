613516637a3b6339e834f85bc6b1eab48820c8ef
duje <dzemunik@yahoo.com>
2024-03-15 12:38:44 +0100

    removed: header.tsx from src/components (removed duplicate, proper header.tsx is in src/common) (FE)

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,7 +3,7 @@ import { Outlet } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { store } from "./store";
 import { getProfileData } from "./store/slices/loggedProfile";
-import { Header } from "./components/header";
+import { Header } from "./components/common/header";
 import { Footer } from "./components/common/footer";
 
 export function App() {
