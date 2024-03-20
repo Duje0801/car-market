ea7313c7ac9c99b3a6cd967e0337c9ee120f6607
duje <dzemunik@yahoo.com>
2024-03-20 21:21:35 +0100

    added: additional responsiveness to src/pages/error/pageDontExist.tsx (FE)

--- client/src/pages/error/pageDontExist.tsx
+++ client/src/pages/error/pageDontExist.tsx
@@ -11,13 +11,13 @@ export function PageDontExist() {
 
   return (
     <div className="flex flex-col gap-4 mt-8">
-      <h2 className="text-2xl text-center w-[65vw] mx-auto">
+      <h2 className="text-2xl text-center w-[65vw] mx-auto xxl:text-3xl">
         Unfortunately, the page you were looking for does not exist.
       </h2>
       <figure>
         <img src={errorImage} alt="error-image" className="w-[65vw] mx-auto md:w-[50vw] lg:w-[35vw]" />
       </figure>
-        <button onClick={handleClick} className="btn bg-black text-white w-[40vw] mx-auto md:w-[30vw] lg:w-[20vw]">Go to home page</button>
+        <button onClick={handleClick} className="btn bg-black text-white w-[40vw] mx-auto md:w-[30vw] lg:w-[20vw] xxl:text-3xl">Go to home page</button>
     </div>
   );
 }
