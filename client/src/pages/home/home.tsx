28682006aa681c8ed6364614f1f919f06a10b2db
duje <dzemunik@yahoo.com>
2024-03-26 18:26:23 +0100

    fixed: responsiveness in src/pages/home/home.tsx (FE)

--- client/src/pages/home/home.tsx
+++ client/src/pages/home/home.tsx
@@ -19,17 +19,17 @@ export function Home() {
       )}
       <div className="flex flex-col lg:flex-row">
         {/*Search form - top/left*/}
-        <div className="flex px-4 lg:w-2/3 lg:pl-16 lg:pr-6 xxl:px-16">
+        <div className="flex px-4 lg:w-2/3 lg:pl-6 lg:pr-4 xxl:pr-16 xxl:pl-6">
           <SearchAds setError={setError} />
         </div>
         {/*Boxes - middle/right*/}
-        <div className="flex flex-col mt-6 gap-6 lg:mt-0 lg:px-4 lg:w-1/3 lg:pr-16 lg:pl-6 xxl:px-16">
+        <div className="flex flex-col mt-6 gap-6 lg:mt-0 lg:px-4 lg:w-1/3 lg:pl-4 lg:pr-6 xxl:pl-16 xxl:pr-6">
           <Box1 />
           <Box2 />
         </div>
       </div>
       {/*5 newest ads*/}
-      <div className="mx-auto w-[90vw] md:w-[70vw] lg:w-full lg:px-16">
+      <div className="mx-auto w-[90vw] md:w-[70vw] lg:w-full lg:px-6">
         <NewestAds />
       </div>
     </div>
